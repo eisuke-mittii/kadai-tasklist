ebf234ddd7ebded642e46b3ff459738c565d96e7
eisuke.mitsu <m.eisuke1229@gmail.com>
2022-01-18 14:44:48 +0000

    modified tasks_conroller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,6 @@ class TasksController < ApplicationController
     end
     
     def show
-        @task = current_user.tasks.find_by(id: params[:id])
     end
     
     def new
@@ -27,12 +26,9 @@ class TasksController < ApplicationController
     end
     
     def edit
-        @task = current_user.tasks.find(params[:id])
     end
     
     def update
-        @task = current_user.tasks.find(params[:id])
-        
         if @task.update(task_params)
           flash[:success] = 'タスクが正常に更新されました'
           redirect_to @task
@@ -43,7 +39,6 @@ class TasksController < ApplicationController
     end
     
     def destroy
-        @task = Task.find(params[:id])
         @task.destroy
         
         flash[:success] = 'タスクは正常に削除されました'
